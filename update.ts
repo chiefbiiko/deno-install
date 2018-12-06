4c17744b6d86a6e7ceea52255bdee3c208a504f8
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-12-06 22:45:54 +0100

    mvp

--- update.ts
+++ update.ts
@@ -1,20 +1,20 @@
-import { 
-  ErrorKind, 
+import {
+  ErrorKind,
   Process,
   ProcessStatus,
   args,
   chmod,
   copyFile,
-  env, 
-  exit, 
-  lstat, 
-  mkdir, 
-  platform, 
+  env,
+  exit,
+  lstat,
+  mkdir,
+  platform,
   symlink,
   writeFile,
   removeAll,
-  run, 
-  makeTempDir 
+  run,
+  makeTempDir
 } from 'deno'
 
 import mkdirp from 'https://raw.githubusercontent.com/chiefbiiko/deno-mkdirp/master/mkdirp.ts'
@@ -42,7 +42,7 @@ const DENO_DIR: string = path_join(get_home(), '.deno')
 const DENO_BIN_DIR: string = path_join(DENO_DIR, 'bin')
 const DENO_BIN: string = path_join(DENO_BIN_DIR, WIN32 ? 'deno.exe' : 'deno')
 const DENO_LINK: string = path_join(
-  WIN32 ? '' : '/usr/local/bin/deno', WIN32 ? 'deno.exe' : 'deno'
+  WIN32 ? '' : '/usr/local/bin', WIN32 ? 'deno.exe' : 'deno'
 )
 
 const LINUX_GZIP: string = 'deno_linux_x64.gz'
@@ -69,7 +69,8 @@ const follow: Function = async (url: string) : Promise<any> => {
   return res
 }
 
-const release_url: Function = async (tag?: string) : Promise<string> => {
+const release_url: Function = 
+  async (tag?: string) : Promise<{ [key: string]: string }> => {
   const url: string = tag ? `${TAG_RELEASE_URL}/${tag}` : LATEST_RELEASE_URL
   var filename: string
   switch (platform.os) {
@@ -82,61 +83,83 @@ const release_url: Function = async (tag?: string) : Promise<string> => {
   const link: string = (await res.text())
     .split(/\r?\n/)
     .find((line: string) => line.includes('href') && line.includes(filename))
-  if (!link) throw Error(`can't find ${filename} @ ${url}`)
+  if (!link) panic(Error(`can't find ${filename} @ ${url}`))
   const match: string = link.replace(/^.*href=(?:"|')([^"']*)(?:"|').*$/, '$1')
-  if (!/^\/denoland/.test(match)) throw Error(`can't find ${filename} @ ${url}`)
-  return `https://github.com${match}`
+  if (!/^\/denoland/.test(match)) panic(Error(`can't find ${filename} @ ${url}`))
+  return {
+    url: `https://github.com${match}`,
+    tag: match.replace(/^.*(v\d+\.\d+\.\d+).*$/, '$1')
+  }
 }
 
-const temp_download: Function = async (temp_dir: string, url: string) : Promise<string> => {
+const temp_download: Function = 
+  async (temp_dir: string, url: string, suffix: string) : Promise<string> => {
   const res: any = await follow(url) // TODO: annotate deno Response
   const arr_buf: ArrayBuffer = await res.arrayBuffer()
-  const temp_file: string = `${temp_dir}/${Date.now()}`
+  const temp_file: string = `${temp_dir}/${Date.now()}.${suffix}`
   await writeFile(temp_file, new Uint8Array(arr_buf))
   return temp_file
 }
 
 const unpack_deno_bin: Function = async (archive: string) : Promise<void> => {
   await mkdirp(DENO_BIN_DIR)
-  const args: string[] = [ platform.os === 'win' ? 'unzip.bat' : 'gunzip' ]
-    .concat([ archive, DENO_BIN_DIR ])
+  let args: string[]
+  if (WIN32) args = [ 'unzip.bat', archive, DENO_BIN_DIR ]
+  else args = [ 'gunzip', '-d', archive ]
   const child: Process = run({ args })
   const child_status: ProcessStatus = await child.status()
-  if (!child_status.success) 
-    throw Error(`(g)unzip failed with code ${child_status.code}`)
+  if (!child_status.success)
+    panic(Error(`(g)unzip failed with code ${child_status.code}`))
+  if (!WIN32) {
+    const gunzipd: string = archive.replace(/\.gz$/, '')
+    await copyFile(gunzipd, DENO_BIN)
+  }
   child.close()
 }
 
 const mk_handy: Function = async () : Promise<void> => {
   await chmod(DENO_DIR, 0o744)
-  await symlink(DENO_BIN, DENO_LINK, WIN32 ? 'file' : undefined)
+  await chmod(DENO_BIN, 0o744)
+  try {
+    await lstat(DENO_LINK)
+  } catch (err) {
+    if (err.kind !== ErrorKind.NotFound) panic(err)
+    await symlink(DENO_BIN, DENO_LINK, WIN32 ? 'file' : undefined)
+  }
 }
 
-const ck_deno: Function = async (expected_version: string) : Promise<void> => {
-  const deno_proc: Process = run({ args: [ 'deno', '--version' ] })
+const ck_deno: Function = async (tag: string) : Promise<void> => {
+  const deno_proc: Process = run({
+    args: [ 'deno', '--version' ],
+    stdout: 'piped'
+  })
   const deno_status: ProcessStatus = await deno_proc.status()
-  if (!deno_status.success) throw Error('update failed')
+  if (!deno_status.success) panic(Error('update failed'))
   const deno_stdout: Uint8Array = new Uint8Array(32)
   while ((await deno_proc.stdout.read(deno_stdout)).nread < 16);
   deno_proc.close()
   const output: string = new TextDecoder('utf-8').decode(deno_stdout)
-  if (!RegExp(expected_version.replace(/\./g, '\\.')).test(output))
-    throw Error('version mismatch')
+  if (!RegExp(tag.replace(/^v/, '').replace(/\./g, '\\.')).test(output))
+    panic(Error('version mismatch'))
 }
 
 const main: Function = async () : Promise<void> => {
-  if (args.some((arg: string) => /^(?:-h|--help)$/.test(arg))) 
+  if (args.some((arg: string) => /^(?:-h|--help)$/.test(arg)))
     return console.log(HELP)
-  console.log('updating deno@latest')
-  const url: string = await release_url(args[1])
+  var tag: string
+  if (/^v\d+\.\d+\.\d+$/.test(args[1])) tag = args[1]
+  pinup(tag ? `updating 2 deno ${tag}` : 'updating 2 deno@latest')
+  const actual: { [key: string]: string } = await release_url(tag)
   const temp_dir: string = await makeTempDir()
-  console.log(`downloading ${url}`)
-  const temp_file: string = await temp_download(temp_dir, url)
+  pinup(`downloading ${actual.url}`)
+  const temp_file: string = 
+    await temp_download(temp_dir, actual.url, WIN32 ? 'zip' : 'gz')
   await unpack_deno_bin(temp_file)
-  console.log('plugging up da binary')
+  pinup('plugging up da binary')
   await mk_handy()
-  await ck_deno()
-  console.log(`update ok`)
+  await ck_deno(actual.tag)
+  await removeAll(temp_dir)
+  pinup(`update ok`)
 }
 
 main()
